368ae8ec05ad05e6f212cdd6e00bb14833a31389
UchihaDSenju <rafiqtariq19@gmail.com>
2024-08-20 12:51:49 +0530

    Controller designed

---
+++ controller/Controller.java
@@ -0,0 +1,16 @@
+package controller;
+import database.*;
+public class Controller {
+    private static volatile Controller controllerInstance = null;
+    private Database db = Database.getDatabaseInstance();
+    
+    private Controller(){}
+
+    public static Controller getControllerInstance(){
+        if(controllerInstance == null){
+            controllerInstance = new Controller();
+        }
+
+        return controllerInstance;
+    }
+}
