796f059b34b1f9dadf3c40492a288a0c0540c7fb
UchihaDSenju <rafiqtariq19@gmail.com>
2024-08-20 12:52:10 +0530

    Database designed

---
+++ database/Database.java
@@ -0,0 +1,20 @@
+package database;
+import java.util.ArrayList;
+
+import model.*;
+public class Database {
+
+    private static volatile Database databaseInstance = null;
+    private ArrayList<Product> products = new ArrayList<>();
+    private ArrayList<Product> transactions = new ArrayList<>();
+    private ArrayList<Product> coupons = new ArrayList<>();
+    private Database(){}
+
+    public static Database getDatabaseInstance(){
+        if(databaseInstance == null){
+            databaseInstance = new Database();
+        }
+        return databaseInstance;
+    }
+    
+}
